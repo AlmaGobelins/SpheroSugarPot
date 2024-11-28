47f2b482d6ea14a3bdc6d053bfc6b5b346820ede
Mathieu <92155925+MathieuDubart@users.noreply.github.com>
2024-11-28 10:55:06 +0100

    :sparkles: initial commit

---
+++ SpheroSugarPot/SpheroSugarPotApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpheroSugarPotApp.swift
+//  SpheroSugarPot
+//
+//  Created by Mathieu Dubart on 28/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct SpheroSugarPotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
